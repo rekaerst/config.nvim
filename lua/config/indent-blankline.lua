1d1d88ebeda3a3e49489f75d5526430efe08f4c9
rekaerst <rekaerst@outlook.com>
2022-03-05 15:57:48 +0800

    removed unused plugins

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,12 +1,16 @@
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=#3a323a gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=#3b3b3b gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guibg=#333b3b gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guibg=#2d3a42 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guibg=#293858 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guibg=#373345 gui=nocombine]]
 
 require("indent_blankline").setup {
-	space_char_blankline = " ",
+	char = "",
 	char_highlight_list = {"IndentBlanklineIndent1", "IndentBlanklineIndent2", "IndentBlanklineIndent3",
-                        "IndentBlanklineIndent4", "IndentBlanklineIndent5", "IndentBlanklineIndent6"}
+                        "IndentBlanklineIndent4", "IndentBlanklineIndent5", "IndentBlanklineIndent6"},
+	space_char_highlight_list = {"IndentBlanklineIndent1", "IndentBlanklineIndent2", "IndentBlanklineIndent3",
+                              "IndentBlanklineIndent4", "IndentBlanklineIndent5", "IndentBlanklineIndent6"},
+	show_trailing_blankline_indent = false,
+	filetype_exclude = {'NvimTree'}
 }
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,15 +1,15 @@
 require 'config.internals'
 require 'config.binding'
 
-require'lspsaga'.setup()
-require'fidget'.setup()
-require'lsp_signature'.setup()
+require 'lsp_signature'.setup{}
 require 'config.cmp'
 require 'config.trouble'
-require 'config.goto-preview'
+require 'lspsaga'.setup{}
+require 'fidget'.setup{}
+require 'config.lint'
 
-require'dapui'.setup()
-require("nvim-dap-virtual-text").setup()
+require 'dapui'.setup{}
+require "nvim-dap-virtual-text".setup{}
 
 require 'config.lualine'
 require 'config.bufferline'
@@ -19,10 +19,10 @@ require 'config.indent-blankline'
 require 'config.tree'
 
 require 'config.treesitter'
-require'spellsitter'.setup()
-require'Comment'.setup()
+require 'spellsitter'.setup{}
+require 'Comment'.setup{}
 
-require'nvim-ts-autotag'.setup()
+require 'nvim-ts-autotag'.setup{}
 require 'config.autopairs'
 require 'config.tabout'
 require 'config.whichkey'
@@ -30,6 +30,6 @@ require 'config.whichkey'
 require 'config.telescope'
 
 require 'config.octo'
-require'neogit'.setup()
+require 'neogit'.setup{}
 
 require 'config.venn'
