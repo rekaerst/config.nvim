8f065ca9107e56cd9bf01ca4559737a84062a1fc
rekaerst <rekaerst@outlook.com>
2022-03-15 03:51:09 +0800

    color: moved location of color.base.colors

--- lua/color/base.lua
+++ lua/color/base.lua
@@ -1,4 +1,24 @@
 local M = {}
+
+M.colors = {
+	base00 = "#1e222a",
+	base01 = "#353b45",
+	base02 = "#3e4451",
+	base03 = "#545862",
+	base04 = "#565c64",
+	base05 = "#abb2bf",
+	base06 = "#b6bdca",
+	base07 = "#c8ccd4",
+	base08 = "#e06c75",
+	base09 = "#d19a66",
+	base0A = "#e5c07b",
+	base0B = "#98c379",
+	base0C = "#56b6c2",
+	base0D = "#61afef",
+	base0E = "#c678dd",
+	base0F = "#be5046",
+}
+
 local function highlight(group, guifg, guibg, attr, guisp)
 	local parts = { group }
 	if guifg then
@@ -209,25 +229,6 @@ local function apply_base16_theme(theme)
 	-- TODO
 	-- nvim.command 'syntax on'
 end
-
-M.colors = {
-	base00 = "#1e222a",
-	base01 = "#353b45",
-	base02 = "#3e4451",
-	base03 = "#545862",
-	base04 = "#565c64",
-	base05 = "#abb2bf",
-	base06 = "#b6bdca",
-	base07 = "#c8ccd4",
-	base08 = "#e06c75",
-	base09 = "#d19a66",
-	base0A = "#e5c07b",
-	base0B = "#98c379",
-	base0C = "#56b6c2",
-	base0D = "#61afef",
-	base0E = "#c678dd",
-	base0F = "#be5046",
-}
 
 function M.setup()
 	apply_base16_theme( M.colors)
