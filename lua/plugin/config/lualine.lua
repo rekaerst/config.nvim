e259b98b8017b24aaf407fad666dae06de54195e
rekaerst <rekaerst@outlook.com>
2022-03-15 04:40:25 +0800

    lualine: use custom theme

--- lua/plugin/config/lualine.lua
+++ lua/plugin/config/lualine.lua
@@ -1,6 +1,36 @@
+local colors = require('color.highlight').colors
 require("lualine").setup({
 	options = {
-		theme = "onedark",
+		theme = {
+			normal = {
+				a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+				b = {bg = colors.one_bg2, fg = colors.white},
+				c = {bg = colors.statusline_bg, fg = colors.light_grey}
+			},
+			insert = {
+				a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+			},
+			visual = {
+				a = {bg = colors.dark_purple, fg = colors.black, gui = 'bold'},
+			},
+			replace = {
+				a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+			},
+			command = {
+				a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+			},
+			inactive = {
+				a = {bg = colors.one_bg2, fg = colors.grey_fg2 },
+			},
+		},
+	},
+	inactive_sections = {
+		lualine_a = {'filename'},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {'location'}
 	},
 	extensions = { "nvim-tree", "toggleterm", "symbols-outline" },
 })
