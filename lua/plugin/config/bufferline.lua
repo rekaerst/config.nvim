65751903be083aba2ac8b526fbbd9fff7c2bdb3d
rekaerst <rekaerst@outlook.com>
2022-03-15 19:13:11 +0800

    bufferline: change diagnostics icon style

--- lua/plugin/config/bufferline.lua
+++ lua/plugin/config/bufferline.lua
@@ -6,8 +6,8 @@ require("bufferline").setup({
 		end,
 		diagnostics = "nvim_lsp",
 		diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			local icon = level:match("error") and " " or " "
-			return " " .. icon .. count
+			local icon = level:match("error") and " " or " "
+			return icon .. count
 		end,
 		max_name_length = 20,
 		tab_size = 16,
