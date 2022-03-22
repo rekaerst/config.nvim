fe0317f00221e727cf9ad348332981b15ac7bad4
rekaerst <rekaerst@outlook.com>
2022-03-22 15:51:56 +0800

    lint: replace cspell with codespell

--- lua/plugin/config/lint.lua
+++ lua/plugin/config/lint.lua
@@ -1,8 +1,11 @@
 local l = require("lint")
 l.linters_by_ft = {
-	python = { "flake8", "cspell" },
-	markdown = { "cspell" },
-	text = { "cspell" },
+	python = { "flake8", "codespell" },
+	c = {"codespell"},
+	cpp = {"codespell"},
+	rust = {"codespell"},
+	markdown = { "codespell" },
+	text = { "codespell" },
 }
 
 vim.cmd([[au BufWritePost <buffer> lua require('lint').try_lint()]])
