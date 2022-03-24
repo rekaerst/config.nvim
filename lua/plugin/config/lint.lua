962401aa9b7f9e085d0339e7385199708ecd4244
rekaerst <rekaerst@outlook.com>
2022-03-25 01:26:28 +0800

    lint: enable linting for c,cpp

--- lua/plugin/config/lint.lua
+++ lua/plugin/config/lint.lua
@@ -1,11 +1,13 @@
 local l = require("lint")
 l.linters_by_ft = {
 	python = { "flake8", "codespell" },
-	c = {"codespell"},
-	cpp = {"codespell"},
-	rust = {"codespell"},
+	c = { "cppcheck", "codespell" },
+	cpp = { "cppcheck", "codespell" },
+	rust = { "codespell" },
 	markdown = { "codespell" },
 	text = { "codespell" },
+	html = { "tidy" },
+	javascript = { "eslint" }
 }
 
 vim.cmd([[au BufWritePost <buffer> lua require('lint').try_lint()]])
