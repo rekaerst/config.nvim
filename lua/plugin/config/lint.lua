166ceba70b557194d28b2480c0a8baddb506de7b
rekaerst <rekaerst@outlook.com>
2022-03-25 21:35:25 +0800

    lint: remove tidy

--- lua/plugin/config/lint.lua
+++ lua/plugin/config/lint.lua
@@ -3,11 +3,11 @@ l.linters_by_ft = {
 	python = { "codespell" },
 	c = { "cppcheck", "codespell" },
 	cpp = { "cppcheck", "codespell" },
+	lua = { "codespell" },
 	rust = { "codespell" },
 	markdown = { "codespell" },
 	text = { "codespell" },
-	html = { "tidy" },
-	javascript = { "eslint" },
+	html = { "codespell" },
+	javascript = { "eslint", "codespell" },
 }
-
 vim.cmd([[au BufWritePost <buffer> lua require('lint').try_lint()]])
