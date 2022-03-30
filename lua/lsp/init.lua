b5bb7c7393ce7aaea57db5f711cf37c7b3fffee3
rekaerst <rekaerst@outlook.com>
2022-03-31 00:29:18 +0800

    lsp: do not format python by default

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -107,3 +107,6 @@ for _, lsp in ipairs(servers) do
 		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 	})
 end
+
+-- disable lspformat when using python
+vim.cmd([[au FileType python :FormatDisable]])
