b8917de82b3eab84144c9d4d397372d8e52a3104
rekaerst <rekaerst@outlook.com>
2022-04-17 11:41:23 +0800

    [color] update highlight on yank cmd

--- lua/color/init.lua
+++ lua/color/init.lua
@@ -1,4 +1,5 @@
 require("color.base").setup()
 require("color.highlight").setup()
 vim.cmd("syntax enable")
-vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank()")
+
+vim.cmd([[au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=150, on_visual=true}]])
