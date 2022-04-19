87f4ab74eb886a2b3c5bfcdb9cfbecb71f4dabbf
rekaerst <rekaerst@outlook.com>
2022-04-19 11:30:16 +0800

    [cmp] chagne confirm mapping from <CR> to <C-y>

--- lua/plugin/config/cmp.lua
+++ lua/plugin/config/cmp.lua
@@ -22,9 +22,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({
-			select = true,
-		}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
