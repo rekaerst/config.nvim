89b30b429a41c3a97b459d75f8695575c0b915d6
rekaerst <rekaerst@outlook.com>
2022-04-22 20:04:21 +0800

    clean up

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,7 +2,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({
+	local packer_bootstrap = fn.system({
 		"git",
 		"clone",
 		"--depth",
