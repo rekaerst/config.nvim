40c046858d076c883c08b8e2209c0cf31c7c89f5
rekaerst <rekaerst@outlook.com>
2022-04-22 16:28:38 +0800

    [ui] make vim.ui.inpput relative to cursor when showing code action

--- lua/plugin/config/dressing.lua
+++ lua/plugin/config/dressing.lua
@@ -1,5 +1,16 @@
 require("dressing").setup({
 	select = {
 		backend = { "builtin" },
+		get_config = function(opt)
+			if opt.kind == "codeaction" then
+				return {
+					backend = "builtin",
+					builtin = {
+						relative = "cursor",
+						min_height = { 1, 0 },
+					},
+				}
+			end
+		end,
 	},
 })
