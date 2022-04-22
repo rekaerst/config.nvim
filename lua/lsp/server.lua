dca081c0b2752fbd3bd2837e9007af927275c0c8
rekaerst <rekaerst@outlook.com>
2022-04-22 17:26:56 +0800

    [lsp] enable snip completion

--- lua/lsp/server.lua
+++ lua/lsp/server.lua
@@ -3,6 +3,7 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 
 -- TODO: remove this after offset encoding issue is fixed
 capabilities.offsetEncoding = { "utf-18" }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- LSP Servers
 local servers = {
