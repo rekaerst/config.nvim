68a0c4dea87d55c9d39609481ffe802b7ee0d313
rekaerst <rekaerst@outlook.com>
2022-04-25 15:36:01 +0800

    treesitter: enable incremental selection

--- lua/plugin/config/treesitter.lua
+++ lua/plugin/config/treesitter.lua
@@ -58,13 +58,20 @@ require("nvim-treesitter.configs").setup({
 		extended_mode = true,
 		max_file_lines = nil,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 	textobjects = {
 		select = {
 			enable = true,
-
 			-- Automatically jump forward to textobj, similar to targets.vim
 			lookahead = true,
-
 			keymaps = {
 				-- You can use the capture groups defined in textobjects.scm
 				["af"] = "@function.outer",
