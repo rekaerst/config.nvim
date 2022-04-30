a1e7f37a8bfa7b2cf83fe5c957520b8d57481988
rekaerst <rekaerst@outlook.com>
2022-04-30 15:48:24 +0800

    feat: enable prettier for json and markdown

--- lua/lsp/null_ls.lua
+++ lua/lsp/null_ls.lua
@@ -13,7 +13,9 @@ function M.setup(on_attach)
 			null_ls.builtins.code_actions.shellcheck,
 			null_ls.builtins.formatting.stylua,
 			null_ls.builtins.formatting.yapf, -- python
-			null_ls.builtins.formatting.markdownlint,
+			null_ls.builtins.formatting.prettierd.with({
+				filetypes = { "json", "markdown" },
+			}),
 			null_ls.builtins.formatting.buf, -- protobuf
 			null_ls.builtins.diagnostics.codespell,
 			null_ls.builtins.diagnostics.flake8,
