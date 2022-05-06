f20d653f31f86a6eb34014d36de33e2963f18100
rekaerst <rekaerst@outlook.com>
2022-05-06 23:57:46 +0800

    feat(plugin): enable cmp-cmdline

--- lua/plugin/config/cmp.lua
+++ lua/plugin/config/cmp.lua
@@ -38,6 +38,7 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 	}),
+
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
@@ -67,4 +68,22 @@ cmp.setup.filetype("gitcommit", {
 	}, { {
 		name = "buffer",
 	} }),
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -139,6 +139,7 @@ local plugins = {
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-emoji",
+			"hrsh7th/cmp-cmdline",
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
 		},
