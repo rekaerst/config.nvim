4737ec730ce71442e848f69916f8b78b84c56d44
rekaerst <rekaerst@outlook.com>
2022-05-08 00:38:48 +0800

    fix(plugin): WhichKey closes Telescope window

--- lua/plugin/config/whichkey.lua
+++ lua/plugin/config/whichkey.lua
@@ -1,4 +1,12 @@
 local wk = require("which-key")
+local show = wk.show
+wk.show = function(keys, opts)
+	if vim.bo.filetype == "TelescopePrompt" then
+		return
+	end
+	show(keys, opts)
+end
+
 wk.setup({
 	plugins = {
 		spelling = {
