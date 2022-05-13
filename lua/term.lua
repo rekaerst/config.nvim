dcd13cb144123498c27306dd813826a2e67334e2
rekaerst <rekaerst@outlook.com>
2022-05-13 15:31:47 +0800

    fix(term): terminal key mapping confilict with programs

--- lua/term.lua
+++ lua/term.lua
@@ -14,11 +14,10 @@ function M.setup()
 
 	function _G.set_terminal_keymaps()
 		local opts = { noremap = true }
-		vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-		vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-		vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-		vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-		vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", [[<C-\><C-h>]], [[<C-\><C-n><C-W>h]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", [[<C-\><C-j>]], [[<C-\><C-n><C-W>j]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", [[<C-\><C-k>]], [[<C-\><C-n><C-W>k]], opts)
+		vim.api.nvim_buf_set_keymap(0, "t", [[<C-\><C-l>]], [[<C-\><C-n><C-W>l]], opts)
 	end
 
 	-- if you only want these mappings for toggle term use term://*toggleterm#* instead
