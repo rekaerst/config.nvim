cd8da804387e87c8e886338961f197b72ed16d41
rekaerst <rekaerst@outlook.com>
2022-05-19 19:42:59 +0800

    feat(lsp): auto open diagnostic float window and highlight symbols under cursor

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,4 +1,5 @@
 local fmt = require("lsp.format")
+local c = require("color.highlight").colors
 
 local signdef = vim.fn.sign_define
 
@@ -7,8 +8,47 @@ local M = {}
 function M.on_attach(client, bufnr)
 	vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 	require("core.mapping").reg_lsp(bufnr)
+	-- formatting
 	if client.supports_method("textDocument/formatting") then
 		fmt.on_attach(client, bufnr)
+	end
+	-- auto open diagnostic float window
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
+	-- highlight symbols under cursor
+	if client.server_capabilities.documentHighlightProvider then
+		vim.highlight.create("LspReferenceRead", { guibg = c.one_bg3 }, false)
+		vim.highlight.create("LspReferenceText", { guibg = c.one_bg3 }, false)
+		vim.highlight.create("LspReferenceWrite", { guibg = c.one_bg3 }, false)
+		vim.api.nvim_create_augroup("lsp_document_highlight", {
+			clear = false,
+		})
+		vim.api.nvim_clear_autocmds({
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+		})
+		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+			group = "lsp_document_highlight",
+			buffer = bufnr,
+			callback = vim.lsp.buf.document_highlight,
+		})
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			group = "lsp_document_highlight",
+			buffer = bufnr,
+			callback = vim.lsp.buf.clear_references,
+		})
 	end
 end
 
