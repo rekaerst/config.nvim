be277571e78fbe305e47783400ff030b1beb167b
rekaerst <rekaerst@outlook.com>
2022-05-22 01:30:24 +0800

    style: move codes to helper functions

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -40,44 +40,9 @@ function M.on_attach(client, bufnr)
 	if client.supports_method("textDocument/formatting") then
 		fmt.on_attach(client, bufnr)
 	end
-	-- auto open diagnostic float window
-	vim.api.nvim_create_autocmd("CursorHold", {
-		buffer = bufnr,
-		callback = function()
-			local opts = {
-				focusable = false,
-				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-				border = "rounded",
-				source = "always",
-				prefix = " ",
-				scope = "cursor",
-			}
-			vim.diagnostic.open_float(nil, opts)
-		end,
-	})
-	-- highlight symbols under cursor
-	if client.server_capabilities.documentHighlightProvider then
-		vim.highlight.create("LspReferenceRead", { guibg = c.one_bg3 }, false)
-		vim.highlight.create("LspReferenceText", { guibg = c.one_bg3 }, false)
-		vim.highlight.create("LspReferenceWrite", { guibg = c.one_bg3 }, false)
-		vim.api.nvim_create_augroup("lsp_document_highlight", {
-			clear = false,
-		})
-		vim.api.nvim_clear_autocmds({
-			buffer = bufnr,
-			group = "lsp_document_highlight",
-		})
-		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-			group = "lsp_document_highlight",
-			buffer = bufnr,
-			callback = vim.lsp.buf.document_highlight,
-		})
-		vim.api.nvim_create_autocmd("CursorMoved", {
-			group = "lsp_document_highlight",
-			buffer = bufnr,
-			callback = vim.lsp.buf.clear_references,
-		})
-	end
+
+	M._auto_open_diagnostic_window(bufnr)
+	M._highlight_symbols_under_cursor(client, bufnr)
 end
 
 function M.setup()
@@ -107,7 +72,52 @@ function M.setup()
 	end
 
 	require("lsp.server").setup(M.on_attach)
-	require("lsp.null_ls").setup(M.on_attach)
+	require("lsp.null_ls").setup(function(client, bufnr)
+		require("core.mapping").reg_lsp(bufnr)
+		fmt.on_attach(client, bufnr)
+	end)
+end
+
+function M._auto_open_diagnostic_window(bufnr)
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
+end
+
+function M._highlight_symbols_under_cursor(client, bufnr)
+	if client.server_capabilities.documentHighlightProvider then
+		vim.highlight.create("LspReferenceRead", { guibg = c.one_bg3 }, false)
+		vim.highlight.create("LspReferenceText", { guibg = c.one_bg3 }, false)
+		vim.highlight.create("LspReferenceWrite", { guibg = c.one_bg3 }, false)
+		vim.api.nvim_create_augroup("lsp_document_highlight", {
+			clear = false,
+		})
+		vim.api.nvim_clear_autocmds({
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+		})
+		vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+			group = "lsp_document_highlight",
+			buffer = bufnr,
+			callback = vim.lsp.buf.document_highlight,
+		})
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			group = "lsp_document_highlight",
+			buffer = bufnr,
+			callback = vim.lsp.buf.clear_references,
+		})
+	end
 end
 
 return M
