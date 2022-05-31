94d46a372fe4bb8e27710bdd8603ef9ded5da164
rekaerst <rekaersp@gmail.com>
2022-06-01 02:34:51 +0800

    refactor(cmp): only auto refresh 3 times if no match is found

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -4,20 +4,28 @@ local autorefresh_blacklist = { "bashls" }
 
 local M = {}
 
-local function refresh()
-	if cmp.visible() then
-		local entry = cmp.get_entries()[1]
-		if #entry.matches == 0 then
-			cmp.complete()
-		end
-	end
-end
-
 function M.auto_refresh(client, bufnr)
+	local refresh_count = 0
+	local refresh = true
+
 	if not vim.tbl_contains(autorefresh_blacklist, client.name) then
 		vim.api.nvim_create_autocmd({ "TextChangedI" }, {
 			callback = function()
-				vim.defer_fn(refresh, 0)
+				vim.defer_fn(function()
+					if cmp.visible() then
+						local entry = cmp.get_entries()[1]
+						if #entry.matches == 0 and refresh then
+							refresh_count = refresh_count + 1
+							if refresh_count >= 3 then
+								refresh = false
+							end
+							cmp.complete()
+						end
+					else
+						refresh_count = 0
+						refresh = true
+					end
+				end, 0)
 			end,
 			buffer = bufnr,
 		})
