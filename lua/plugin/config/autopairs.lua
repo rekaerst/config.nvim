fef4f50a95039baec2b3ac54b77883c8ca6a639d
rekaerst <rekaersp@gmail.com>
2022-06-04 02:14:30 +0800

    chore(autopairs): migrate upstream changes

--- lua/plugin/config/autopairs.lua
+++ lua/plugin/config/autopairs.lua
@@ -1,6 +1,22 @@
-require("nvim-autopairs").setup({
-	diable_filetype = { "TelescopePrompt", "clap_input" },
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+
+npairs.setup({
 	check_ts = true,
 	enable_check_bracket_line = true,
-	fast_wap = {},
+	fast_wrap = {
+		map = "<M-e>",
+	},
+})
+
+-- insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+-- arrow key on javascript
+npairs.add_rules({
+	Rule("%(.*%)%s*%=>$", " {  }", { "typescript", "typescriptreact", "javascript" })
+		:use_regex(true)
+		:set_end_pair_length(2),
 })
